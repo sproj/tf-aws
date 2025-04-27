49cf41b1b5041c5da2d7fa506b220cc554b8f694
sproj <jonesalan404@gmail.com>
2025-04-27 12:40:35 +0100

    correct kubernetes-ec2 module source path

--- examples/dev-kubernetes-ec2/main.tf
+++ examples/dev-kubernetes-ec2/main.tf
@@ -1,5 +1,5 @@
 module "kubernetes_ec2" {
-  source = "../../terraform/modules/deployment-types/kubernetes-ec2"
+  source = "../../modules/deployment-types/kubernetes-ec2"
 
   aws_region         = "eu-west-1"
   name_prefix        = "dev-k8s"
