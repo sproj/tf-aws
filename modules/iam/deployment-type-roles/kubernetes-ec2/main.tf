dbbd546ae39f0da8418091082d2bb46208bcc526
sproj <jonesalan404@gmail.com>
2025-04-27 18:44:36 +0100

    rm unused variable

--- modules/iam/deployment-type-roles/kubernetes-ec2/main.tf
+++ modules/iam/deployment-type-roles/kubernetes-ec2/main.tf
@@ -1,20 +1,17 @@
 module "creator" {
   source = "./creator"
 
-  name_prefix            = var.name_prefix
   bootstrapper_user_name = var.bootstrapper_user_name
 }
 
 module "manager" {
   source = "./manager"
 
-  name_prefix            = var.name_prefix
   bootstrapper_user_name = var.bootstrapper_user_name
 }
 
 module "reader" {
   source = "./reader"
 
-  name_prefix            = var.name_prefix
   bootstrapper_user_name = var.bootstrapper_user_name
 }
--- modules/iam/deployment-type-roles/kubernetes-ec2/reader/variables.tf
+++ modules/iam/deployment-type-roles/kubernetes-ec2/reader/variables.tf
@@ -1,8 +1,3 @@
-variable "name_prefix" {
-  description = "Prefix for resource naming and access control."
-  type        = string
-}
-
 variable "bootstrapper_user_name" {
   description = "IAM user allowed to assume this role."
   type        = string
