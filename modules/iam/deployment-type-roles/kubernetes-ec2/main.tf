820b52c59e70734db3f160a9873d53f5b4d68fdc
sproj <jonesalan404@gmail.com>
2025-04-28 19:31:00 +0100

    extract policy per resource type for each of k8s-ec2 type roles

--- modules/iam/deployment-type-roles/kubernetes-ec2/main.tf
+++ modules/iam/deployment-type-roles/kubernetes-ec2/main.tf
@@ -12,23 +12,38 @@ data "terraform_remote_state" "state_backend" {
 module "creator" {
   source = "./creator"
 
-  bootstrapper_role_name         = var.bootstrapper_role_name
-  backend_full_access_policy_arn = data.terraform_remote_state.state_backend.outputs.terraform_backend_full_access_policy_arn
-  networking_allowed_actions     = local.kubernetes_ec2_networking_actions.creator
+  bootstrapper_role_name               = var.bootstrapper_role_name
+  backend_full_access_policy_arn       = data.terraform_remote_state.state_backend.outputs.terraform_backend_full_access_policy_arn
+  networking_allowed_actions           = local.kubernetes_ec2_allowed_actions.networking.creator
+  ec2_allowed_actions                  = local.kubernetes_ec2_allowed_actions.ec2.creator
+  elasticloadbalancing_allowed_actions = local.kubernetes_ec2_allowed_actions.elasticloadbalancing.creator
+  autoscaling_allowed_actions          = local.kubernetes_ec2_allowed_actions.autoscaling.creator
+  iam_allowed_actions                  = local.kubernetes_ec2_allowed_actions.iam.creator
+  ecr_allowed_actions                  = local.kubernetes_ec2_allowed_actions.ecr.creator
 }
 
 module "manager" {
   source = "./manager"
 
-  bootstrapper_role_name         = var.bootstrapper_role_name
-  backend_full_access_policy_arn = data.terraform_remote_state.state_backend.outputs.terraform_backend_full_access_policy_arn
-  networking_allowed_actions     = local.kubernetes_ec2_networking_actions.manager
+  bootstrapper_role_name               = var.bootstrapper_role_name
+  backend_full_access_policy_arn       = data.terraform_remote_state.state_backend.outputs.terraform_backend_full_access_policy_arn
+  networking_allowed_actions           = local.kubernetes_ec2_networking_actions.manager
+  ec2_allowed_actions                  = local.kubernetes_ec2_allowed_actions.ec2.manager
+  elasticloadbalancing_allowed_actions = local.kubernetes_ec2_allowed_actions.elasticloadbalancing.manager
+  autoscaling_allowed_actions          = local.kubernetes_ec2_allowed_actions.autoscaling.manager
+  iam_allowed_actions                  = local.kubernetes_ec2_allowed_actions.iam.manager
+  ecr_allowed_actions                  = local.kubernetes_ec2_allowed_actions.ecr.manager
 }
 
 module "reader" {
   source = "./reader"
 
-  bootstrapper_role_name             = var.bootstrapper_role_name
-  backend_readonly_access_policy_arn = data.terraform_remote_state.state_backend.outputs.terraform_backend_readonly_access_policy_arn
-  networking_allowed_actions         = local.kubernetes_ec2_networking_actions.reader
+  bootstrapper_role_name               = var.bootstrapper_role_name
+  backend_readonly_access_policy_arn   = data.terraform_remote_state.state_backend.outputs.terraform_backend_readonly_access_policy_arn
+  networking_allowed_actions           = local.kubernetes_ec2_networking_actions.reader
+  ec2_allowed_actions                  = local.kubernetes_ec2_allowed_actions.ec2.reader
+  elasticloadbalancing_allowed_actions = local.kubernetes_ec2_allowed_actions.elasticloadbalancing.reader
+  autoscaling_allowed_actions          = local.kubernetes_ec2_allowed_actions.autoscaling.reader
+  iam_allowed_actions                  = local.kubernetes_ec2_allowed_actions.iam.reader
+  ecr_allowed_actions                  = local.kubernetes_ec2_allowed_actions.ecr.reader
 }
--- modules/iam/deployment-type-roles/kubernetes-ec2/manager/main.tf
+++ modules/iam/deployment-type-roles/kubernetes-ec2/manager/main.tf
@@ -18,34 +18,15 @@ resource "aws_iam_role" "kubernetes_ec2_manager" {
   }
 }
 
-resource "aws_iam_policy" "kubernetes_ec2_manager_policy" {
-  name = "k8s-ec2-manager-policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowModifyResources"
-        Effect = "Allow"
-        Action = [
-          "ec2:ModifyInstanceAttribute",
-          "ec2:ModifyVolume",
-          "elasticloadbalancing:ConfigureHealthCheck",
-          "elasticloadbalancing:ModifyListener",
-          "elasticloadbalancing:ModifyLoadBalancerAttributes",
-          "autoscaling:UpdateAutoScalingGroup",
-          "iam:PassRole"
-        ]
-        Resource = ["*"]
-      }
-    ]
-  })
-
-  tags = {
-    ManagedBy = "${data.aws_caller_identity.current.arn}"
-  }
+resource "aws_iam_role_policy_attachment" "manager_backend_access" {
+  role       = aws_iam_role.kubernetes_ec2_manager.name
+  policy_arn = var.backend_full_access_policy_arn
 }
 
+
+data "aws_caller_identity" "current" {}
+
+# manager networking access
 data "aws_iam_policy_document" "kubernetes_ec2_manager_networking_access" {
   statement {
     sid       = "AllowNetworkingManagementActions"
@@ -55,6 +36,7 @@ data "aws_iam_policy_document" "kubernetes_ec2_manager_networking_access" {
   }
 }
 
+# manager networking policy
 resource "aws_iam_policy" "kubernetes_ec2_manager_networking_policy" {
   name        = "kubernetes-ec2-manager-networking-policy"
   description = "Policy allowing kubernetes-ec2-manager to manage networking resources"
@@ -65,19 +47,143 @@ resource "aws_iam_policy" "kubernetes_ec2_manager_networking_policy" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "manager_attach" {
-  role       = aws_iam_role.kubernetes_ec2_manager.name
-  policy_arn = aws_iam_policy.kubernetes_ec2_manager_policy.arn
-}
-
-resource "aws_iam_role_policy_attachment" "manager_backend_access" {
-  role       = aws_iam_role.kubernetes_ec2_manager.name
-  policy_arn = var.backend_full_access_policy_arn
-}
-
+# attach manager networking policy
 resource "aws_iam_role_policy_attachment" "manager_networking_access" {
   role       = aws_iam_role.kubernetes_ec2_manager.name
-  policy_arn = aws_iam_policy.kubernetes_ec2_manager_policy
+  policy_arn = aws_iam_policy.kubernetes_ec2_manager_networking_policy
 }
 
-data "aws_caller_identity" "current" {}
+# manager ec2 actions
+data "aws_iam_policy_document" "kubernetes_ec2_manager_ec2_access" {
+  statement {
+    sid       = "AllowEC2CreationActions"
+    effect    = "Allow"
+    actions   = var.ec2_allowed_actions
+    resources = ["*"]
+  }
+}
+
+# manager ec2 policy
+resource "aws_iam_policy" "kubernetes_ec2_manager_ec2_policy" {
+  name        = "kubernetes-ec2-manager-ec2-policy"
+  description = "Policy allowing kubernetes-ec2-manager to create ec2 resources"
+  policy      = data.aws_iam_policy_document.kubernetes_ec2_manager_ec2_access.json
+
+  tags = {
+    ManagedBy = "${data.aws_caller_identity.current.arn}"
+  }
+}
+# attach creator ec2 policy
+resource "aws_iam_role_policy_attachment" "manager_attach" {
+  role       = aws_iam_role.kubernetes_ec2_manager.name
+  policy_arn = aws_iam_policy.kubernetes_ec2_manager_ec2_policy.arn
+}
+
+# manager elasticloadbalancing actions
+data "aws_iam_policy_document" "kubernetes_ec2_manager_elasticloadbalancing_access" {
+  statement {
+    sid       = "AllowElasticLoadbalancingCreationActions"
+    effect    = "Allow"
+    actions   = var.elasticloadbalancing_allowed_actions
+    resources = ["*"]
+  }
+}
+
+# manager elasticloadbalancing policy
+resource "aws_iam_policy" "kubernetes_ec2_manager_elasticloadbalancing_policy" {
+  name        = "kubernetes-ec2-manager-elasticloadbalancing-policy"
+  description = "Policy allowing kubernetes-ec2-manager to manage elasticloadbalancing resources"
+  policy      = data.aws_iam_policy_document.kubernetes_ec2_manager_elasticloadbalancing_access.json
+
+  tags = {
+    ManagedBy = "${data.aws_caller_identity.current.arn}"
+  }
+}
+
+# attach manager elasticloadbalancing policy
+resource "aws_iam_role_policy_attachment" "manager_attach" {
+  role       = aws_iam_role.kubernetes_ec2_manager.name
+  policy_arn = aws_iam_policy.kubernetes_ec2_manager_elasticloadbalancing_policy.arn
+}
+
+# manager autoscaling actions
+data "aws_iam_policy_document" "kubernetes_ec2_manager_autoscaling_access" {
+  statement {
+    sid       = "AllowAutoScalingCreationActions"
+    effect    = "Allow"
+    actions   = var.autoscaling_allowed_actions
+    resources = ["*"]
+  }
+}
+
+# manager autoscaling policy
+resource "aws_iam_policy" "kubernetes_ec2_manager_autoscaling_policy" {
+  name        = "kubernetes-ec2-creator-autoscaling-policy"
+  description = "Policy allowing kubernetes-ec2-manager to manage autoscaling resources"
+  policy      = data.aws_iam_policy_document.kubernetes_ec2_manager_autoscaling_access.json
+
+  tags = {
+    ManagedBy = "${data.aws_caller_identity.current.arn}"
+  }
+}
+
+# attach manager autoscaling policy
+resource "aws_iam_role_policy_attachment" "manager_attach" {
+  role       = aws_iam_role.kubernetes_ec2_manager.name
+  policy_arn = aws_iam_policy.kubernetes_ec2_manager_autoscaling_policy.arn
+}
+
+# manager iam actions
+data "aws_iam_policy_document" "kubernetes_ec2_manager_iam_access" {
+  statement {
+    sid       = "AllowIAMCreationActions"
+    effect    = "Allow"
+    actions   = var.iam_allowed_actions
+    resources = ["*"]
+  }
+}
+
+# manager iam policy
+resource "aws_iam_policy" "kubernetes_ec2_manager_iam_policy" {
+  name        = "kubernetes-ec2-manager-iam-policy"
+  description = "Policy allowing kubernetes-ec2-manager to manage iam resources"
+  policy      = data.aws_iam_policy_document.kubernetes_ec2_manager_iam_access.json
+
+  tags = {
+    ManagedBy = "${data.aws_caller_identity.current.arn}"
+  }
+}
+
+# attach manager iam policy
+resource "aws_iam_role_policy_attachment" "manager_attach" {
+  role       = aws_iam_role.kubernetes_ec2_manager.name
+  policy_arn = aws_iam_policy.kubernetes_ec2_manager_iam_policy.arn
+}
+
+# manager ecr actions
+data "aws_iam_policy_document" "kubernetes_ec2_manager_ecr_access" {
+  statement {
+    sid       = "AllowECRCreationActions"
+    effect    = "Allow"
+    actions   = var.iam_allowed_actions
+    resources = ["*"]
+  }
+}
+
+# manager ecr policy
+resource "aws_iam_policy" "kubernetes_ec2_manager_ecr_policy" {
+  name        = "kubernetes-ec2-manager-ecr-policy"
+  description = "Policy allowing kubernetes-ec2-manager to manage ecr resources"
+  policy      = data.aws_iam_policy_document.kubernetes_ec2_manager_ecr_access.json
+
+  tags = {
+    ManagedBy = "${data.aws_caller_identity.current.arn}"
+  }
+}
+
+# attach manager ecr policy
+resource "aws_iam_role_policy_attachment" "manager_attach" {
+  role       = aws_iam_role.kubernetes_ec2_manager.name
+  policy_arn = aws_iam_policy.kubernetes_ec2_manager_ecr_policy.arn
+}
+
