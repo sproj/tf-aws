74838c75aca60a0408cadc1becf19916f68e1256
sproj <jonesalan404@gmail.com>
2025-05-16 22:26:16 +0100

    deployment-type should aggregate outputs of its submodules has to be a better way to pipe these out

--- modules/deployment-types/kubernetes-ec2/outputs.tf
+++ modules/deployment-types/kubernetes-ec2/outputs.tf
@@ -1,9 +1,39 @@
 output "vpc_id" {
   description = "ID of the created VPC"
-  value       = aws_vpc.this.id
+  value       = module.networking.vpc_id
 }
 
-output "public_subnet_id" {
-  description = "ID of the public subnet"
-  value       = aws_subnet.public.id
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = module.networking.public_subnet_ids
+}
+
+output "node_security_group_ids" {
+  description = "Security group IDs for Kubernetes worker nodes"
+  value       = module.security_groups.node_sg_ids
+}
+
+output "master_security_group_ids" {
+  description = "Security group IDs for Kubernetes master node"
+  value       = module.security_groups.master_sg_ids
+}
+
+output "instance_profile_name" {
+  description = "IAM instance profile name for EC2 nodes"
+  value       = module.iam_instance_profile.profile_name
+}
+
+output "node_asg_name" {
+  description = "Auto Scaling Group name for worker nodes"
+  value       = module.ec2_nodes.asg_name
+}
+
+output "master_instance_id" {
+  description = "Instance ID of the Kubernetes master node"
+  value       = module.ec2_master.instance_id
+}
+
+output "master_public_ip" {
+  description = "Public IP address of the Kubernetes master node"
+  value       = module.ec2_master.public_ip
 }
