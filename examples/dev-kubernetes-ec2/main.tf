60615f368ba6d2b9e710811ac64a981bf6b28013
sproj <jonesalan404@gmail.com>
2025-05-16 22:27:01 +0100

    cleanup

--- examples/dev-kubernetes-ec2/main.tf
+++ examples/dev-kubernetes-ec2/main.tf
@@ -22,34 +22,12 @@ module "kubernetes_ec2" {
   public_subnet_cidr_blocks = ["10.10.1.0/24"]
   availability_zones        = ["eu-west-1a"]
   name_prefix               = "dev-k8s"
-  node_ami_id = "ami-09079da11cd2861fa" # ubuntu 22.04 LTS (eu-west-1) - check https://cloud-images.ubuntu.com/locator/ec2/
+  key_name                  = "dev-k8s-key"
+  node_ami_id               = "ami-09079da11cd2861fa" # ubuntu 22.04 LTS (eu-west-1) - check https://cloud-images.ubuntu.com/locator/ec2/
+  master_ami_id             = "ami-09079da11cd2861fa"
   tags = {
     Environment    = "dev"
     DeploymentType = "kubernetes-ec2"
     ManagedBy      = "terraform"
   }
 }
-
-# module "networking" {
-#   source = "../../modules/deployment-types/kubernetes-ec2/networking"
-
-#   name_prefix               = "dev-k8s"
-#   vpc_cidr_block            = "10.0.0.0/16"
-#   public_subnet_cidr_blocks = ["10.0.1.0/24"]
-#   availability_zones        = ["eu-west-1a"]
-
-#   tags = {
-#     Environment    = "dev"
-#     DeploymentType = "kubernetes-ec2"
-#     ManagedBy      = "terraform"
-#   }
-# }
-
-## only need outputs which come from a deployment module if they are intended to be used here.
-# output "vpc_id" {
-#   value = module.networking.vpc_id
-# }
-
-# output "public_subnet_ids" {
-#   value = module.networking.public_subnet_ids
-# }
