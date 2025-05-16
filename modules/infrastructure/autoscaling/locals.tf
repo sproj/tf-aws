183520ec76e8ae8d3f714099db934953d6c37e28
sproj <jonesalan404@gmail.com>
2025-05-16 22:27:30 +0100

    add many missing permissions to infra modules

--- modules/infrastructure/autoscaling/locals.tf
+++ modules/infrastructure/autoscaling/locals.tf
@@ -2,10 +2,36 @@ locals {
   autoscaling_permissions = {
     creator = [
       "autoscaling:CreateAutoScalingGroup",
+      "autoscaling:UpdateAutoScalingGroup",
       "autoscaling:DeleteAutoScalingGroup",
+      "autoscaling:CreateLaunchConfiguration",
+      "autoscaling:DeleteLaunchConfiguration",
+      "autoscaling:UpdateAutoScalingGroup",
+      "autoscaling:AttachInstances",
+      "autoscaling:DetachInstances",
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "autoscaling:PutScalingPolicy",
+      "autoscaling:DeletePolicy",
+      "autoscaling:PutNotificationConfiguration",
+      "autoscaling:DeleteNotificationConfiguration",
+      "autoscaling:SetInstanceProtection",
+      "autoscaling:SuspendProcesses",
+      "autoscaling:ResumeProcesses",
+      "autoscaling:DescribeLifecycleHooks",
+      "autoscaling:PutLifecycleHook",
+      "autoscaling:DeleteLifecycleHook",
+      "autoscaling:CompleteLifecycleAction",
+      "autoscaling:RecordLifecycleActionHeartbeat",
+      "autoscaling:Describe*"
     ]
     manager = [
       "autoscaling:UpdateAutoScalingGroup",
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:AttachInstances",
+      "autoscaling:DetachInstances",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "autoscaling:Describe*"
     ]
     reader = [
       "autoscaling:Describe*",
--- modules/infrastructure/iam/locals.tf
+++ modules/infrastructure/iam/locals.tf
@@ -1,10 +1,27 @@
 locals {
   iam_permissions = {
     creator = [
-      "iam:PassRole"
+      "iam:PassRole",
+      "iam:CreateRole",
+      "iam:TagRole",
+      "iam:GetRole",
+      "iam:DeleteRole",
+      "iam:ListRolePolicies",
+      "iam:ListAttachedRolePolicies",
+      "iam:ListInstanceProfilesForRole",
+      "iam:CreateInstanceProfile",
+      "iam:TagInstanceProfile",
+      "iam:GetInstanceProfile",
+      "iam:DeleteInstanceProfile",
+      "iam:AddRoleToInstanceProfile",
+      "iam:RemoveRoleFromInstanceProfile"
     ]
     manager = [
-      "iam:PassRole"
+      "iam:PassRole",
+      "iam:GetRole",
+      "iam:ListRolePolicies",
+      "iam:ListAttachedRolePolicies",
+      "iam:ListInstanceProfilesForRole"
     ]
     reader = [
       "iam:Get*",
