d0583671619514f5f16f5f4b986c5792cc9c3d13
sproj <jonesalan404@gmail.com>
2025-05-17 14:05:06 +0100

    dev-k8s name prefix to var derive ssh key name

--- examples/dev-kubernetes-ec2/outputs.tf
+++ examples/dev-kubernetes-ec2/outputs.tf
@@ -3,26 +3,22 @@ output "master_public_ip" {
   value       = module.kubernetes_ec2.master_public_ip
 }
 
-# harcoded ssh key name - should be a var
 output "ssh_command" {
   description = "SSH command to connect to the master node"
-  value       = "ssh -i ~/.ssh/dev-k8s-key ubuntu@${module.kubernetes_ec2.master_public_ip}"
+  value       = "ssh -i ~/.ssh/${var.name_prefix}-key ubuntu@${module.kubernetes_ec2.master_public_ip}"
 }
 
-# harcoded ssh key name - should be a var
 output "start_k8s_tunnel_command" {
   description = "Command to start the SSH tunnel for kubectl access"
-  value       = "bash start-k8s-tunnel.sh ${module.kubernetes_ec2.master_public_ip} ${module.kubernetes_ec2.master_private_ip} dev-k8s-key 6443"
+  value       = "bash start-k8s-tunnel.sh ${module.kubernetes_ec2.master_public_ip} ${module.kubernetes_ec2.master_private_ip} ${var.name_prefix}-key 6443"
 }
 
-# harcoded ssh key name - should be a var
 output "scp_kube_config_file" {
   description = "Copy kube config file from master node to local AFTER cluster setup complete"
-  value = "scp -i ~/.ssh/dev-k8s-key ubuntu@${module.kubernetes_ec2.master_public_ip}:/home/ubuntu/.kube/config ~/.kube/config"
+  value = "scp -i ~/.ssh/${var.name_prefix}-key ubuntu@${module.kubernetes_ec2.master_public_ip}:/home/ubuntu/.kube/config ~/.kube/config"
 }
 
-# harcoded ssh key name - should be a var
 output "check_initialization_command" {
   description = "Command to check the initialization status of the cluster"
-  value       = "./check-k8s-init.sh ${module.kubernetes_ec2.master_public_ip} ~/.ssh/dev-k8s-key"
+  value       = "./check-k8s-init.sh ${module.kubernetes_ec2.master_public_ip} ~/.ssh/${var.name_prefix}-key"
 }
