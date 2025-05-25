2e994affe05c6a58c1cf0b2d24be592c0c007f5f
sproj <jonesalan404@gmail.com>
2025-05-25 09:43:59 +0100

    worker_node layer -> deploy worker_node not base_infra

--- examples/dev-kubernetes-ec2/layered/worker_nodes/main.tf
+++ examples/dev-kubernetes-ec2/layered/worker_nodes/main.tf
@@ -15,13 +15,36 @@ provider "aws" {
   profile = "kubernetes-ec2-creator"
 }
 
-module "base_infrastructure" {
-  source                    = "../../../../modules/deployment-types/kubernetes-ec2/base_infrastructure"
-  vpc_cidr_block            = "10.10.0.0/16"
-  public_subnet_cidr_blocks = ["10.10.1.0/24"]
-  availability_zones        = ["eu-west-1a"]
-  allowed_ssh_cidr          = "${var.allowed_ssh_cidr}/32"
+# Pull outputs from base_infrastructure layer
+data "terraform_remote_state" "base_infrastructure" {
+  backend = "s3"
+  config = {
+    bucket  = "tfaws-dev-state-backend"
+    key     = "examples/kubernetes-ec2/base_infrastructure/terraform.tfstate"
+    region  = "eu-west-1"
+    profile = "kubernetes-ec2-creator"
+  }
+}
+
+# Pull outputs from control_plane layer
+data "terraform_remote_state" "control_plane" {
+  backend = "s3"
+  config = {
+    bucket  = "tfaws-dev-state-backend"
+    key     = "examples/kubernetes-ec2/control_plane/terraform.tfstate"
+    region  = "eu-west-1"
+    profile = "kubernetes-ec2-creator"
+  }
+}
+
+module "worker_nodes" {
+  source                    = "../../../../modules/deployment-types/kubernetes-ec2/worker_nodes"
+  public_subnet_ids         = data.terraform_remote_state.base_infrastructure.outputs.public_subnet_ids
+  security_group_ids        = [data.terraform_remote_state.base_infrastructure.outputs.master_security_group_id]
+  iam_instance_profile_name = data.terraform_remote_state.base_infrastructure.outputs.instance_profile_name
+  ami_id                    = var.ami_id
   name_prefix               = var.name_prefix
+  instance_type = "t3.micro"
   tags = {
     Environment    = "dev"
     DeploymentType = "kubernetes-ec2"
