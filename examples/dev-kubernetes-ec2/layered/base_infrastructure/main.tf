b11c670864c88414093d60093c89a130542e0a10
sproj <jonesalan404@gmail.com>
2026-04-10 16:13:44 +0100

    add cluster secret store ESO operator backed by ssm ParameterStore secrets are SecretStrings added manually to that store secrets are namespaced under, eg, `/dev/dev-k8s/*` ESO adds k8s secret objects to cluster from external store client application refrences those secrets from external store service acc running ec2 nodes gets policy allowing ssm parameter actions (currently wide open) - this is the account from which credentials are sought by ESO to read the parameter store.

--- examples/dev-kubernetes-ec2/layered/base_infrastructure/main.tf
+++ examples/dev-kubernetes-ec2/layered/base_infrastructure/main.tf
@@ -20,6 +20,11 @@ module "ebs_csi_policy" {
   name_prefix = var.name_prefix
 }
 
+module "ssm_read_policy" {
+  source      = "../../../../modules/infrastructure/ssm-read-policy"
+  name_prefix = var.name_prefix
+}
+
 module "base_infrastructure" {
   source                          = "../../../../modules/deployment-types/kubernetes-ec2/base_infrastructure"
   vpc_cidr_block                  = "10.10.0.0/16"
@@ -28,7 +33,7 @@ module "base_infrastructure" {
   allowed_ssh_cidr                = "${var.allowed_ssh_cidr}/32"
   name_prefix                     = var.name_prefix
   env                             = var.env
-  additional_instance_policy_arns = [module.ebs_csi_policy.policy_arn]
+  additional_instance_policy_arns = [module.ebs_csi_policy.policy_arn, module.ssm_read_policy.policy_arn]
   tags = {
     Environment    = "dev"
     DeploymentType = "kubernetes-ec2"
---
+++ modules/infrastructure/ssm-read-policy/main.tf
@@ -0,0 +1,24 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "ssm_read" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath",
+      "ssm:DescribeParameters",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "ssm_read" {
+  name        = "${var.name_prefix}-ssm-read-policy"
+  description = "Permissions for the EC2 instance role running k8s nodes to interact with the Parameter Store for use with External Secret Operator"
+  policy      = data.aws_iam_policy_document.ssm_read.json
+
+  tags = {
+    ManagedBy = data.aws_caller_identity.current.arn
+  }
+}
