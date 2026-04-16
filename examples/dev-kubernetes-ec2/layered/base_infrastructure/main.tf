fa467a4615d22f7808598c89fb7928b9812fb27d
sproj <jonesalan404@gmail.com>
2026-04-16 15:10:59 +0100

    update ssm policy module with ssm_write

--- examples/dev-kubernetes-ec2/layered/base_infrastructure/main.tf
+++ examples/dev-kubernetes-ec2/layered/base_infrastructure/main.tf
@@ -20,23 +20,29 @@ module "ebs_csi_policy" {
   name_prefix = var.name_prefix
 }
 
-module "ssm_read_policy" {
-  source      = "../../../../modules/infrastructure/ssm-read-policy"
+module "ssm_policies" {
+  source      = "../../../../modules/infrastructure/ssm-policies"
   name_prefix = var.name_prefix
+  env         = var.env
 }
 
 module "base_infrastructure" {
-  source                          = "../../../../modules/deployment-types/kubernetes-ec2/base_infrastructure"
-  vpc_cidr_block                  = "10.10.0.0/16"
-  public_subnet_cidr_blocks       = ["10.10.1.0/24"]
-  availability_zones              = ["eu-west-1a"]
-  allowed_ssh_cidr                = "${var.allowed_ssh_cidr}/32"
-  name_prefix                     = var.name_prefix
-  env                             = var.env
-  subnet_tags                     = { "kubernetes.io/role/elb" : 1, "kubernetes.io/cluster/${var.name_prefix}" : "shared" }
-  additional_instance_policy_arns = [module.ebs_csi_policy.policy_arn, module.ssm_read_policy.policy_arn]
+  source                    = "../../../../modules/deployment-types/kubernetes-ec2/base_infrastructure"
+  vpc_cidr_block            = "10.10.0.0/16"
+  public_subnet_cidr_blocks = ["10.10.1.0/24"]
+  availability_zones        = ["eu-west-1a"]
+  allowed_ssh_cidr          = "${var.allowed_ssh_cidr}/32"
+  name_prefix               = var.name_prefix
+  env                       = var.env
+  subnet_tags               = { "kubernetes.io/role/elb" : 1, "kubernetes.io/cluster/${var.name_prefix}" : "shared" }
+  additional_instance_policy_arns = [
+    module.ebs_csi_policy.policy_arn,
+    module.ssm_policies.ssm_read_policy_arn,
+    module.ssm_policies.ssm_write_policy_arn
+  ]
+
   tags = {
-    Environment    = "dev"
+    Environment    = "${var.env}"
     DeploymentType = "kubernetes-ec2"
     ManagedBy      = "terraform"
   }
