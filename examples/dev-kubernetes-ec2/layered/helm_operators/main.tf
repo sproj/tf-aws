a4753b00ac24d393919e9f9a412363d715783fe6
sproj <jonesalan404@gmail.com>
2026-04-18 12:48:52 +0100

    add ebs-csi-driver installation to helm_operators

--- examples/dev-kubernetes-ec2/layered/helm_operators/main.tf
+++ examples/dev-kubernetes-ec2/layered/helm_operators/main.tf
@@ -81,6 +81,7 @@ provider "kubernetes" {
   client_key             = base64decode(data.aws_ssm_parameter.client_key.value)
 }
 
+# Install ESO
 resource "kubernetes_namespace" "external_secrets_namespace" {
   metadata {
     name = "external-secrets"
@@ -115,4 +116,53 @@ resource "helm_release" "external_secrets" {
   chart      = "external-secrets"
   name       = "external-secrets"
   depends_on = [kubernetes_namespace.external_secrets_namespace, kubernetes_secret.eso_reader_aws_credentials]
+}
+
+resource "kubernetes_manifest" "cluster_secret_store" {
+  depends_on = [helm_release.external_secrets]
+  manifest   = yamldecode(file("./eso/cluster-secret-store.yaml"))
+}
+
+# Install EBS CSI driver
+data "aws_ssm_parameter" "ebs_csi_ec2_access_key_id" {
+  name            = "/${var.env}/${var.name_prefix}/ebs-csi-ec2-access-key-id"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "ebs_csi_ec2_secret_access_key" {
+  name            = "/${var.env}/${var.name_prefix}/ebs-csi-ec2-secret-access-key"
+  with_decryption = true
+}
+
+resource "kubernetes_secret" "ebs_csi_driver_credentials" {
+  data = {
+    "key_id" : data.aws_ssm_parameter.ebs_csi_ec2_access_key_id.value,
+    "access_key" : data.aws_ssm_parameter.ebs_csi_ec2_secret_access_key.value
+  }
+
+  metadata {
+    namespace = "kube-system"
+    name      = "aws-secret"
+  }
+}
+
+resource "helm_release" "ebs_csi_driver" {
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  chart      = "aws-ebs-csi-driver"
+  name       = "aws-ebs-csi-driver"
+  depends_on = [kubernetes_secret.ebs_csi_driver_credentials]
+  values     = [file("./ebs-csi/ebs-csi-values.yaml")]
+}
+
+resource "kubernetes_storage_class_v1" "ebs_csi_storageclass" {
+  depends_on = [helm_release.ebs_csi_driver]
+  metadata {
+    name = "ebs-sc"
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  volume_binding_mode = "WaitForFirstConsumer"
+  parameters = {
+    "type" : "gp3"
+  }
 }
