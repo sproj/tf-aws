741f480fa1f6aae8d2fd6230da6920478c847441
sproj <jonesalan404@gmail.com>
2026-04-18 15:18:10 +0100

    aws load balancer controller installation

--- examples/dev-kubernetes-ec2/layered/helm_operators/main.tf
+++ examples/dev-kubernetes-ec2/layered/helm_operators/main.tf
@@ -201,3 +201,18 @@ resource "kubernetes_manifest" "cert_manager_cluster_issuer" {
   depends_on = [helm_release.cert_manager_chart]
   manifest   = yamldecode(file("./cert-manager/cluster-issuer.yaml"))
 }
+
+# Install AWS Load Balancer Controller
+resource "kubernetes_manifest" "aws_lbc_external_secrets" {
+  depends_on = [kubernetes_manifest.cluster_secret_store]
+  manifest   = yamldecode(file("./aws-lbc/aws-lbc-external-secrets.yaml"))
+}
+
+resource "helm_release" "aws_lbc_chart" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  depends_on = [kubernetes_manifest.aws_lbc_external_secrets]
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  values     = [file("./aws-lbc/aws-lbc-values.yaml")]
+}
