a4dec1ee1d899cca4b22eab9659a0be98c4947dd
sproj <jonesalan404@gmail.com>
2026-04-20 19:55:41 +0100

    add cluster_databases

---
+++ examples/dev-kubernetes-ec2/layered/cluster_databases/postgres.tf
@@ -0,0 +1,92 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "tfaws-dev-state-backend"
+    key            = "examples/kubernetes-ec2/cluster_databases/terraform.tfstate"
+    region         = "eu-west-1"
+    profile        = "kubernetes-ec2-creator"
+    dynamodb_table = "tfaws-dev-lock-backend"
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = "kubernetes-ec2-creator"
+}
+
+# Read the TLS credentials written to SSM by master-runtime.sh at cluster init
+data "aws_ssm_parameter" "cluster_ca_certificate" {
+  name            = "/${var.env}/${var.name_prefix}/k8s-api/cluster_ca_certificate"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "client_certificate" {
+  name            = "/${var.env}/${var.name_prefix}/k8s-api/client_certificate"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "client_key" {
+  name            = "/${var.env}/${var.name_prefix}/k8s-api/client_key"
+  with_decryption = true
+}
+
+# Read the public and private IPs written by the control_plane layer
+data "terraform_remote_state" "control_plane" {
+  backend = "s3"
+  config = {
+    bucket  = "tfaws-dev-state-backend"
+    key     = "examples/kubernetes-ec2/control_plane/terraform.tfstate"
+    region  = "eu-west-1"
+    profile = "kubernetes-ec2-creator"
+  }
+}
+
+# Avoid repeating these across both provider blocks
+locals {
+  k8s_host            = "https://${data.terraform_remote_state.control_plane.outputs.master_public_ip}:6443"
+  k8s_tls_server_name = data.terraform_remote_state.control_plane.outputs.master_private_ip
+}
+
+provider "kubernetes" {
+  host                   = local.k8s_host
+  tls_server_name        = local.k8s_tls_server_name
+  cluster_ca_certificate = base64decode(data.aws_ssm_parameter.cluster_ca_certificate.value)
+  client_certificate     = base64decode(data.aws_ssm_parameter.client_certificate.value)
+  client_key             = base64decode(data.aws_ssm_parameter.client_key.value)
+}
+
+resource "kubernetes_namespace" "databases_namespace" {
+  metadata {
+    name = "databases"
+  }
+}
+
+resource "kubernetes_manifest" "postgres_external_secrets" {
+  depends_on = [kubernetes_namespace.databases_namespace]
+  manifest   = yamldecode(file("./manifests/postgres-external-secrets.yaml"))
+}
+
+resource "kubernetes_manifest" "postgres_stateful_set" {
+  depends_on = [kubernetes_namespace.databases_namespace]
+  manifest   = yamldecode(file("./manifests/postgres-statefulset.yaml"))
+}
+
+resource "kubernetes_manifest" "postgres_headless_service" {
+  depends_on = [kubernetes_namespace.databases_namespace]
+  manifest   = yamldecode(file("./manifests/headless-service.yaml"))
+}
+
+resource "kubernetes_manifest" "postgres_service" {
+  depends_on = [kubernetes_namespace.databases_namespace]
+  manifest   = yamldecode(file("./manifests/postgres-service.yaml"))
+}
