6a1bb9b9abf3612508b7fc936605a3fa298487ef
sproj <jonesalan404@gmail.com>
2026-05-04 13:18:55 +0100

    add url shortener rabbitmq credentials

---
+++ bootstrap/rabbit-mq-credentials/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = "super-user"
+}
+
+resource "random_password" "rabbitmq_user_password" {
+  length           = 32
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_ssm_parameter" "rabbit_mq_username" {
+  name  = "/${var.env}/${var.name_prefix}/url-shortener/rabbitmq-username"
+  type  = "SecureString"
+  value = "postgres"
+}
+
+resource "aws_ssm_parameter" "rabbitmq_user_password" {
+  name  = "/${var.env}/${var.name_prefix}/url-shortener/rabbitmq-password"
+  type  = "SecureString"
+  value = random_password.rabbitmq_user_password.result
+}
